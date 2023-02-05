a5643cf01fa25c85b05158c51c9026067dd35810
dimsonnGH <dimsonn@mail.ru>
2023-02-05 12:11:47 +0400

    add DialogFlow to VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -3,12 +3,22 @@ import os
 import random
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
+from dialog_flow import detect_intent_text
+
+
+load_dotenv()
+GOOGLE_PROJECT_ID = os.getenv("GOOGLE_PROJECT_ID")
 
 
 def echo(event, vk_api):
+    session_id = event.user_id
+    text = event.text
+    language_code = "ru"
+    google_response = detect_intent_text(GOOGLE_PROJECT_ID, session_id, text, language_code)
+
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=google_response,
         random_id=random.randint(1,1000)
     )
 

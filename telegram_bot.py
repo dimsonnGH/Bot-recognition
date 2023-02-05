eca6f55e417e4280b9e2e144c26232b67059e671
dimsonnGH <dimsonn@mail.ru>
2023-02-05 12:02:16 +0400

    transferring google dialogflow procedures to a separate file

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from dialog_flow import detect_intent_text
 
 load_dotenv()
 GOOGLE_PROJECT_ID = os.getenv("GOOGLE_PROJECT_ID")
@@ -38,28 +39,6 @@ def echo(update: Update, context: CallbackContext) -> None:
     google_response = detect_intent_text(GOOGLE_PROJECT_ID, session_id, text, language_code)
     update.message.reply_text(google_response)
 
-
-def detect_intent_text(project_id, session_id, text, language_code):
-    """Returns the result of detect intent with texts as inputs.
-
-    Using the same `session_id` between requests allows continuation
-    of the conversation."""
-    from google.cloud import dialogflow
-
-    session_client = dialogflow.SessionsClient()
-
-    session = session_client.session_path(project_id, session_id)
-    # print("Session path: {}\n".format(session))
-
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
-    query_input = dialogflow.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    return response.query_result.fulfillment_text
 
 
 def implicit():
